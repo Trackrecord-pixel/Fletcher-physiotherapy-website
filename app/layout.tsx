5ff548b7f297d807ab23a43150fece02a9f6c1ea
Trackrecord-pixel <info@fletcherphysiotherapy.com.au>
2026-08-20 23:04:58 +1000

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en-AU">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600;9..144,700&family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <OrganizationSchema />
         <WebSiteSchema />
